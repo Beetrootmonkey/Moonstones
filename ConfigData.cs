c3c5a12298ea17338707709b356571f586025236
Beetrootmonkey <n.adelhard@googlemail.com>
2018-08-10 20:03:15 +0200

    fixed stuff i forgot to fix

---
+++ ConfigData.cs
@@ -0,0 +1,85 @@
+﻿using HamstarHelpers.Utilities.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moonstones
+{
+    class ConfigData : ConfigurationDataBase
+    {
+        public readonly static Version ConfigVersion = new Version(1, 2, 6);
+        public readonly static string ConfigFileName = "The Lunatic Config.json";
+
+
+        ////////////////
+
+        public string VersionSinceUpdate = "";
+
+        public bool Enabled = true;
+
+        public int DaysUntil = 9;  // Days until The End
+        public int HalfDaysRecoveredPerMask = 4;    // Half days recovered per mask
+        public float WallOfFleshMultiplier = 2.5f;    // Added time for WoF kill
+        public float HardModeMultiplier = 1.5f; // Added time for hard mode bosses
+
+        public bool LoonyEnforcesBossSequence = true;
+        public bool LoonyAcceptsMasksWithoutBossKill = false;
+        public bool LoonySellsSummonItems = true;
+        public bool LoonyShunsCheaters = false;
+        public bool LoonyGivesCompletionReward = true;
+        public bool LoonyIndicatesDaysRemaining = true;
+
+        public bool OnlyVanillaBossesDropMasks = false;
+        public bool MoonLordMaskWins = false;
+
+        public int DEBUGFLAGS = 0;  // 1: Display info, 2: Fast time, 4: Reset, 8: Reset win, 16: Skip to signs, 32: Display net info
+
+
+
+        ////////////////
+
+        public bool UpdateToLatestVersion()
+        {
+            var new_config = new LunaticConfigData();
+            var vers_since = this.VersionSinceUpdate != "" ?
+                new Version(this.VersionSinceUpdate) :
+                new Version();
+
+            if (vers_since >= LunaticConfigData.ConfigVersion)
+            {
+                return false;
+            }
+
+            if (vers_since < new Version(1, 2, 2))
+            {
+                if (this.DaysUntil == LunaticConfigData._1_2_1_DaysUntil)
+                {
+                    this.DaysUntil = new_config.DaysUntil;
+                }
+                if (this.HardModeMultiplier == LunaticConfigData._1_2_1_HardModeMultiplier)
+                {
+                    this.HardModeMultiplier = new_config.HardModeMultiplier;
+                }
+                if (this.HalfDaysRecoveredPerMask == LunaticConfigData._1_2_1_HalfDaysRecoveredPerMask)
+                {
+                    this.HalfDaysRecoveredPerMask = new_config.HalfDaysRecoveredPerMask;
+                }
+            }
+
+            this.VersionSinceUpdate = LunaticConfigData.ConfigVersion.ToString();
+
+            return true;
+        }
+
+
+        ////////////////
+
+        public string _OLD_SETTINGS_BELOW = "";
+
+        public readonly static int _1_2_1_DaysUntil = 9;
+        public readonly static float _1_2_1_HardModeMultiplier = 1.5f;
+        public readonly static int _1_2_1_HalfDaysRecoveredPerMask = 4;
+    }
+}
--- Items/MoonstoneAccessory.cs
+++ Items/MoonstoneAccessory.cs
@@ -17,51 +17,20 @@ namespace Moonstones.Items
         {
             item.value = 10000;
 			item.rare = 2;
-<<<<<<< Updated upstream
-            item.maxStack = 1;
-            item.SetNameOverride("Moonstone");
-        }
-
-=======
 			item.maxStack = 1;
             item.accessory = true;
 		}
 
 		public override int ChoosePrefix(UnifiedRandom rand)
 		{
-		return (byte)rand.Next(62, 81);
+		    return (byte)rand.Next(62, 81);
 		}
->>>>>>> Stashed changes
-
-
-<<<<<<< Updated upstream
-        public override int ChoosePrefix(UnifiedRandom rand)
-        {
-            return (byte)rand.Next(62, 84);
-        }
 
         public override bool CanRightClick()
         {
             return true;
         }
 
-        public override void RightClick(Player player)
-        {
-            Item item = player.HeldItem;
-            if (item != null && item.accessory)
-            {
-                item.prefix = this.item.prefix;
-            }
-            else
-            {
-                int number = Item.NewItem((int)player.position.X, (int)player.position.Y, player.width, player.height, this.item.type, 1, false, this.item.prefix, false, false);
-                if (Main.netMode == 1)
-                {
-                    NetMessage.SendData(21, -1, -1, null, number, 1f, 0f, 0f, 0, 0, 0);
-                }
-            }
-        }
-=======
 		public override void RightClick(Player player)
 		{
 			Item item = player.HeldItem;
@@ -78,6 +47,5 @@ namespace Moonstones.Items
 				}
 			}
 		}
->>>>>>> Stashed changes
 	}
 }
